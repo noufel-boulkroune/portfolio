cf55dcf30b530a1654c7f12c75da545774381dbe
noufel-boulkroune <noufelboulkroune@gmail.com>
2025-01-19 17:13:37 +0100

    Change the theme to a dark one

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   plugins: [require("tailwind-scrollbar")],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFD700",
+        secondary: "#000000",
+        dark: "#1A1A1A",
+        light: "#F5F5F5",
+      },
+    },
   },
 };
