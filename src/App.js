b3dd4f1e761bf0c89c7408a19115a1070d54a211
noufel-boulkroune <noufelboulkroune@gmail.com>
2025-09-19 16:58:41 +0100

    Add analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Portfolio from "./Portfolio";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <Portfolio />
+      <Analytics />
     </div>
   );
 }
